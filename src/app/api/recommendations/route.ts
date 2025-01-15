ff8e1b2aecf901b2f29f545e0d7c14860e3b0995
takuto yuki <ppppp303@gmail.com>
2025-01-15 13:43:21 +0900

    Update Delete symbol

--- src/app/api/recommendations/route.ts
+++ src/app/api/recommendations/route.ts
@@ -11,16 +11,6 @@ export async function POST(request: NextRequest) {
 
     const recommendations = await getRecommendations(country, city, answers);
 
-    // recommendations　ex
-    // 1. Destination: **Kyoto**
-    //   Reason: Kyoto offers a harmonious blend of rich cultural heritage, historical sites, and serene natural surroundings. With its famous temples, traditional gardens, and proximity to scenic spots like Arashiyama, it provides a peaceful suburban atmosphere ideal for walking in nature. Kyoto's warm climate and traditional Japanese cuisine also align well with your preferences.
-    //
-    // 2. Destination: **Kanazawa**
-    //   Reason: Kanazawa is a quiet city that retains much of its historical charm, featuring stunning gardens like Kenrokuen, traditional tea houses, and a slower-paced lifestyle. It offers a balance of natural beauty and cultural richness, making it an ideal place for you to enjoy both outdoor activities and cultural exploration. The city's relaxed, suburban vibe makes it a good fit for your preference for peace and tranquility.
-    //
-    // 3. Destination: **Queenstown** (New Zealand)
-    //   Reason: Queenstown is renowned for its breathtaking natural landscapes, with towering mountains, lakes, and forests that offer endless outdoor activities. Known as the adventure capital, it’s a great place for enjoying nature, hiking, and a peaceful atmosphere, while the small town fosters close-knit community interactions. This destination provides a unique lifestyle with a balance of adventure and serenity in a warm, sunny environment.
-
     if (!recommendations) {
       return NextResponse.json({ error: 'Failed to generate recommendations' }, { status: 500 });
     }
@@ -28,7 +18,7 @@ export async function POST(request: NextRequest) {
     const recommendationsCites: RecommendationsCity[] = recommendations.split('\n\n').map((recommendation: string) => {
       const [city, reasons] = recommendation.split('\n');
       return {
-        name: city.split('Destination: ')[1],
+        name: city.split('Destination: ')[1].replace('*', ''),
         reasons: reasons.split('Reason: ')[1],
         images: [""],
       };
